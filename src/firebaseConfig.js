db7940c8d5cff12f5a7870f664ef33d77f9d669d
Gaurav Raj Ghimire <grg.grg.47@gmail.com>
2020-09-27 22:56:21 -0400

    firebase Added

---
+++ src/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyBmJvLwU8PMfglyZN5xS1D_8_snfxMf8RE",
+    authDomain: "eventmanagement-1.firebaseapp.com",
+    databaseURL: "https://eventmanagement-1.firebaseio.com",
+    projectId: "eventmanagement-1",
+    storageBucket: "eventmanagement-1.appspot.com",
+    messagingSenderId: "448769087430",
+    appId: "1:448769087430:web:62176c499bf67b678c44c7"
+}
